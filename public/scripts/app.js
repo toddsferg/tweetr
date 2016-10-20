20fedcd8d510d07c4bd4b2d1c611465fd6d34fbe
Todd Ferguson <toddsferg@gmail.com>
2016-10-20 22:22:55 +0000

    added post and get from /tweets, css a mess

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,57 +4,19 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function(){
- var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
+
+
+function renderTweets(array){
+  for( var i = 0; i < array.length; i++){
+    var currentTweet = array[i];
+    var $newTweet = createTweetElement(currentTweet);
+    $('.feeder').append($newTweet)
   }
-];
+}
 
-
-
-function createTweetElement(tw){
-
+function createTweetElement(tweetData){
+  var time = new Date;
+  console.log(time);
 
   var $freshTweet = $("<article>").addClass("tweeted");
   var $header = $('<header>');
@@ -62,9 +24,9 @@ function createTweetElement(tw){
   var $name = $('<p>').addClass("chosen-name").text(tweetData.user.name)
   var $handle = $('<p>').addClass("handle").text(tweetData.user.handle);
   var $midspace = $('<div>').addClass("midspace");
-  var $tweettext = $('<p>').addClass("tweettext").text("tweetData.user.content")
+  var $tweettext = $('<p>').addClass("tweettext").text(tweetData.content.text)
   var $footer = $('<footer>').addClass("footer");
-  var $dateSent = $('<p>').addClass("dateSent").text("10 days ago");
+  var $dateSent = $('<p>').addClass("dateSent").text(time);
   var $iconHeart = $("<i>").attr({"class": "fa fa-heart", "aria-hidden": "true"});
   var $iconRetweet = $("<i>").attr({"class": "fa fa-retweet", "aria-hidden": "true"});
   var $iconFlag = $("<i>").attr({"class": "fa fa-font-awesome", "aria-hidden": "true"});
@@ -74,19 +36,38 @@ function createTweetElement(tw){
   $footer = $footer.append($dateSent).append($iconFlag).append($iconRetweet).append($iconHeart);
 
   $freshTweet = $freshTweet.append($header).append($midspace).append($footer);
-
-
-  $('.feeder').append($freshTweet);
-
-//   $header = $header.append('$avatar').append('$name').append('handle');
-//   $footer = $footer.append('$dateSent').append($iconHeart).append($iconRetweet).append($iconFlag);
-//   $freshTweet = $freshTweet.append($header).append('$tweettext').append('$footer');
-//   return $freshTweet;
-//   console.log($freshtweet)
+  var $tweet = $('.feeder').append($freshTweet);
+  return $tweet;
  }
 
+//Hijax
 
- })
+$('form').on("submit", function(event){
+  event.preventDefault();
+  $.ajax({
+    method: 'post',
+    url: '/tweets',
+    dataType: 'json',
+    data: $(this).serialize()
+    // success:function(data){
+    //   console.log(data);
+
+  });
+});
+
+  function loadTweets(){
+    event.preventDefault();
+   $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      data: $(this).serialize(),
+      dataType: 'json'
+    }).done(function(data){
+      renderTweets(data);
+    })
+  }loadTweets();
+
+})
 
 
 
